69457450ebc40ddb8c04ad1aa678bde8dc092376
GraphZou <906520989@qq.com>
2024-10-29 12:08:58 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+
+  <el-button plain @click="dialogVisible = true">
+    Open a draggable Dialog
+  </el-button>
+
+  <el-dialog v-model="dialogVisible" title="Tips" width="500" draggable>
+    <span>It's a draggable Dialog</span>
+    <template #footer>
+      <div class="dialog-footer">
+        <el-button @click="dialogVisible = false">Cancel</el-button>
+        <el-button type="primary" @click="dialogVisible = false">
+          Confirm
+        </el-button>
+      </div>
+    </template>
+  </el-dialog>
+
+</template>
+ 
+<script setup>
+import { ref } from 'vue'
+const dialogVisible = ref(false);
+</script>
