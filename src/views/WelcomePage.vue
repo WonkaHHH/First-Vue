926cb442ab2c6d931db25252afc1643489091d11
GraphZou <906520989@qq.com>
2024-10-31 02:50:05 +0800

    整理公告

--- src/views/WelcomePage.vue
+++ src/views/WelcomePage.vue
@@ -11,15 +11,11 @@
   <el-row :gutter="20" justify="start" align="center"> <el-button type="primary">退出游戏</el-button> </el-row>
 
   <el-row :gutter="20" justify="end" align="center">
-  <el-button type="primary" @click="dialogVisible = true">游戏公告</el-button>
-  <el-dialog v-model="dialogVisible" title="游戏公告" width="500" draggable>
-    <span>内容内容内容内容</span>
-    <template #footer>
-      <div class="dialog-footer">
-        <el-button @click="dialogVisible = false">Cancel</el-button>
-        <el-button type="primary" @click="dialogVisible = false">Confirm</el-button>
-      </div>
-    </template>
+  <el-button type="primary" @click="dialogVisible = true">公告</el-button>
+  <el-dialog v-model="dialogVisible" title="公告" width="500" draggable>
+    <div>一些版本更新信息</div>
+    <div>QQ群号：</div>
+    <div>QQ群号二维码</div>
   </el-dialog>
   </el-row>
 
@@ -38,17 +34,13 @@ import { ref } from 'vue';
 export default {
   setup() {
     const router = useRouter();
-    const dialogVisible = ref(false); // 定义响应式数据
+    const dialogVisible = ref(true); // 定义响应式数据
 
     const goToSecondPage = () => {
       router.push({ name: 'MainPage' });
     };
 
-    const openDialog = () => {
-      dialogVisible.value = true; // 打开弹窗
-    };
-
-    return { goToSecondPage, dialogVisible, openDialog }; // 返回响应式数据和方法
+    return { goToSecondPage, dialogVisible }; // 返回响应式数据和方法
   },
 };
 
