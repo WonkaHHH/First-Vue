0b91638188e05b28c0c5193731323963d012646b
GraphZou <906520989@qq.com>
2024-11-01 21:29:37 +0800

    添加button

--- src/views/MainPage.vue
+++ src/views/MainPage.vue
@@ -1,5 +1,20 @@
 <template>
-  <el-button type="primary" @click="goToHomePage">Go Back to Home Page</el-button>
+<el-row justify="center" align="center">
+  <el-button type="primary" size="medium">运行</el-button>
+  <el-button type="primary" size="small">1</el-button>
+  <el-button type="primary" size="small">2</el-button>
+  <el-button type="primary" size="small">3</el-button>
+  <el-button type="primary" size="small">4</el-button>
+  <el-button type="primary" size="small">5</el-button>
+  <el-button type="primary" size="small">6</el-button>
+  <el-button type="primary" size="small">7</el-button>
+  <el-button type="primary" size="small">8</el-button>
+  <el-button type="primary" size="small">9</el-button>
+  <el-button type="primary" size="small">0</el-button>
+  <el-button type="primary" size="large">背包</el-button>
+  <el-button type="primary" size="medium">设置</el-button>
+  <el-button type="primary" @click="goToHomePage">返回菜单</el-button>
+</el-row>
 </template>
 
 <script>
